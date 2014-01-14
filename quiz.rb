7006f85c2fc586c39e00ad4a76d179643b8ac245
Prescott Murphy <prurph@gmail.com>
2014-01-14 09:30:54 -0500

    Finished everything?

--- quiz.rb
+++ quiz.rb
@@ -11,7 +11,9 @@
 # You can sleep in if it is not a weekday or if you are on vacation.
 # sleep_in? => false
 # sleep_in?(vacation: true) => true
-def sleep_in?
+def sleep_in?(options={})
+  return true if options[:vacation] == true # Specify true so :vacation == 4 doesn't trigger
+  return false unless (Time.now.wday == 6 || Time.now.wday == 7)
 end
 
 # Question 2: a method called del_del
@@ -19,6 +21,9 @@ end
 # Remove "del" from a string.
 # del_del("abdelcd") => "abcd"
 # del_del("xyz") => "xyz"
+def del_del(str)
+  str.gsub(/del/, '')
+end
 
 # Question 3: a method called missing_char
 #############
@@ -26,6 +31,11 @@ end
 # If you don't enter a string
 # missing_char("kitten", 1) => "ktten"
 # missing_char(347, 1) => RuntimeError: Please enter a string!
+def missing_char(str, indx)
+  raise RuntimeError if str.class != String
+  str[indx] = ''
+  str
+end
 
 # Question 4: a method called near_hundred
 #############
@@ -35,9 +45,17 @@ end
 # 93.near_hundred? => true
 # near_hundred(52) => false
 # near_hundred('two') => RuntimeError: Please enter a number!
+def near_hundred(num)
+  raise RuntimeError if num.class != Fixnum
+  (90..99).to_a.include?(num)
+end
 
 # Question 5: a method called back_around
 #############
 # Given a string, move the last character to the beginning.
 # "cat".back_around => "tca"
 # "hello".back_around => "ohell"
+def back_around(str)
+  last = str[str.length - 1]
+  last + str.chop
+end
--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -32,6 +32,18 @@ end
   # Example Usage:
   # del_del("abdelcd") => "abcd"
   # del_del("xyz") => "xyz"
+describe '#del_del' do
+  it 'returns abcd if passed abelcd' do
+    expect(del_del("abdelcd")).to eq "abcd"
+  end
+
+  it 'returns original if no del matched' do
+    expect(del_del("xyz")).to eq "xyz"
+  end
+
+end
+
+
 
 # Question 3: a method called missing_char
   # Example Usage:
@@ -39,13 +51,38 @@ end
   # missing_char(347, 1) => RuntimeError: Please enter a string!
   # https://www.relishapp.com/rspec/rspec-expectations/v/2-14/docs/built-in-matchers/raise-error-matcher
 
+describe '#missing_char' do
+  it 'deletes character at index of a string' do
+    expect(missing_char("kitten", 1)).to eq "ktten"
+  end
+  it 'raises an error when passed a non-string' do
+    expect{ missing_char(347, 1) }.to raise_error(RuntimeError)
+  end
+end
+
 # Question 4: a method called near_hundred
   # Example Usage:
   # near_hundred(93) => true
   # near_hundred(52) => false
   # near_hundred('two') => RuntimeError: Please enter a number!
+describe '#near_hundred' do
+  it 'returns true when number between 90 and 99' do
+    expect(near_hundred(93)).to be true
+  end
+  it 'returns false when number not between 90 and 99' do
+    expect(near_hundred(52)).to be false
+  end
+  it 'raises RuntimeError when not passed a Fixnum' do
+    expect {near_hundred('two')}.to raise_error(RuntimeError)
+  end
+end
 
 # Question 5: a method called back_around
   # Example Usage:
   # "cat".back_around => "tca"
   # "hello".back_around => "ohell"
+describe '#back_around' do
+  it 'returns a string with the last index in front' do
+    expect(back_around("cat")).to eq "tca"
+  end
+end
