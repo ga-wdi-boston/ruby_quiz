83d037850d43428e83c2abb2303139f613026e85
Yiyang Song <songyiyang2012@gmail.com>
2014-01-14 09:30:36 -0500

    Yiyang

--- quiz.rb
+++ quiz.rb
@@ -11,7 +11,13 @@
 # You can sleep in if it is not a weekday or if you are on vacation.
 # sleep_in? => false
 # sleep_in?(vacation: true) => true
-def sleep_in?
+def sleep_in?(option_hash)
+	if option_hash[:vacation] == true 
+		return true
+	elsif Time.now.day % 7 == 0 || Time.now.day % 6 == 0
+		return true
+	else return false
+	end
 end
 
 # Question 2: a method called del_del
@@ -19,6 +25,18 @@ end
 # Remove "del" from a string.
 # del_del("abdelcd") => "abcd"
 # del_del("xyz") => "xyz"
+def del_del (string)
+	str = ''
+	del_del.split('').each do |char|
+		if char != 'd' && char != 'e' && cahr != 'l'
+			str = str + char
+		end
+	end
+		return str
+end
+
+
+
 
 # Question 3: a method called missing_char
 #############
@@ -26,6 +44,17 @@ end
 # If you don't enter a string
 # missing_char("kitten", 1) => "ktten"
 # missing_char(347, 1) => RuntimeError: Please enter a string!
+
+def missing_char(string, number)
+	if string.class != "String"
+		puts "RuntimeError: Please enter a string!"
+	else
+	a = string.split('')
+	a[number] = nil
+	b = a.compact
+	return b.to_s
+	end
+end		
 
 # Question 4: a method called near_hundred
 #############
@@ -35,9 +64,27 @@ end
 # 93.near_hundred? => true
 # near_hundred(52) => false
 # near_hundred('two') => RuntimeError: Please enter a number!
+def near_hundred (number)
+	if number.class != "Fixnum"
+		puts "RuntimeError: Please enter a number!"
+	elsif number >= 90 && number <= 99
+		return true
+	elsif number < 90
+		return false
+	end 
+end
+
+
 
 # Question 5: a method called back_around
 #############
 # Given a string, move the last character to the beginning.
 # "cat".back_around => "tca"
 # "hello".back_around => "ohell"
+
+def back_around(string)
+	a = string.split('')	
+	b = a.pop
+	a.unshift(b)
+	return a
+end
